7a16a810a3210ce8e18ade7db5b2cb39e8b5d2de
richardgmcmahon <rgm@ast.cam.ac.uk>
2018-06-09 19:42:37 +0100

    modified:   wsdb_query.py

--- wsdb_query.py
+++ wsdb_query.py
@@ -1,31 +1,44 @@
-"""
+""""
+
+
+TODO: look at the W1-W2 colours for all Gaia pairs; also look at WISE
+nion-detections
+
+https://www.ast.cam.ac.uk/ioa/wikis/WSDB/index.php/WSDB_GAIADR2
+gaia_dr2.gaia_source
+
+See here
+https://www.ast.cam.ac.uk/ioa/wikis/WSDB/index.php/WSDB_GAIADR2_AUX#Neighbor_table
+
+wsdb=> select * from gaia_dr2_aux.gaia_source_neighbour_count limit 5;
+
+      source_id      |        ra        |        dec        | cnt0_25 | cnt0_50 | cnt1 | cnt2 | cnt4 | cnt8 | cnt16 | cnt32 | cnt64 | cnt128
+---------------------+------------------+-------------------+---------+---------+------+------+------+------+-------+-------+-------+--------
+ 4164666032716788352 | 268.158775207367 | -8.79686399880187 |       1 |       1 |    1 |    1 |    1 |    4 |    10 |    36 |   151 |    584
+ 4164666032721252864 | 268.162921175373 |  -8.7992920387406 |       1 |       1 |    1 |    1 |    4 |    5 |    12 |    41 |   147 |    599
+ 4164666032716765056 |  268.16327175346 | -8.79868137404748 |       1 |       1 |    1 |    2 |    4 |    4 |    11 |    36 |   151 |    600
+ 4164666028420860160 | 268.163489504561 | -8.79893229296021 |       1 |       1 |    1 |    2 |    4 |    4 |    11 |    37 |   150 |    597
+ 4164666032721250816 | 268.163979711806 | -8.79930172774628 |       1 |       1 |    1 |    1 |    4 |    4 |    11 |    38 |   149 |    595
+(5 rows)
+
 
 Requires:
 https://github.com/segasai/astrolibpy/tree/master/utils
 
 
+
+
+
+Requires:
+https://github.com/segasai/astrolibpy/tree/master/utils
+
 this is the easyaccess git commit that adds the SQL to the FITS format
 output file:
+
 https://github.com/mgckind/easyaccess/commit/aa1ea0adfc059993ff5dfb72fd156237d5088b4d
 
 fits[1].write_history('Created by easyaccess ' + version.__version__ + ' on ' + created)
 fits[1].write_comment('Query = ' + query)
-
-eg
-
-HISTORY Created by easyaccess 1.3.1 on 2016-Dec-17 02:18:55                     COMMENT Query = SELECT *  FROM     SVA1_COADD WHERE     (ra BETWEEN 33.0 AND  38COMMENT .0) AND     (dec BETWEEN -8.0 AND -3.0)
-
-better if something like
-
-HISTORY Created by easyaccess 1.3.1 on 2016-Dec-17 02:18:55                     COMMENT STARTQUERY
-COMMENT SELECT
-COMMENT     *
-COMMENT FROM
-COMMENT     SVA1_COADD
-COMMENT WHERE
-COMMENT     (ra BETWEEN 33.0 AND  38.0) AND
-COMMENT     (dec BETWEEN -8.0 AND -3.0)
-COMMENT ENDQUERY
 
 
 WISE columns are described here:
@@ -71,21 +84,27 @@ These look worth looking at too and a few aperture for W1 and W2; ignore
 W3 and W4 aperture magniutdes for now
 
 
+
+
 """
 
 from __future__ import print_function
 
 # standard library functions
+import time
+t0 = time.time()
+
 import os
 import sys
-import time
+
 import datetime
 import argparse
+
+import sqlparse
 
 # 3rd party functions
 # import atpy
 # print(atpy.__version__)
-
 from astropy.table import Table
 from astropy.io import fits
 import pandas as pd
@@ -93,16 +112,54 @@ import pandas as pd
 # private functions
 sys.path.append('/home/rgm/soft/python/lib/')
 from librgm import sqlutil
+# print(sqlutil.__version__)
 # help(sqlutil)
 from librgm.wsdb import rd_config_wsdb
 from librgm.table_metadata import table_metadata
 
-
-# print(sqlutil.__version__)
-
-t0=time.time()
 # print time to load all the python functions
 print('Elapsed time(secs): ', time.time() - t0)
+
+
+
+def table_add_sqlquery(table=None, query=None):
+    """
+
+    common indentation is removed with textwrap.dedent()
+
+    import textwrap
+    query = textwrap.dedent(query)
+
+    """
+
+    def cutquery(query, length):
+        """
+        Return query in a list of fixed sized character strings
+
+        suggest length = 70 so can be stored in FITS format COMMENT
+        from easyaccess codebase
+        """
+
+        result = [query[0 + i:length + i] for i in range(0, len(query), length)]
+
+        return result
+
+    table.meta['SQL'] = query
+    comment = []
+    comment.append('START:SQLQUERY')
+    for iline, line in enumerate(query.splitlines()):
+        print(iline, ':', line)
+        cutresult = cutquery(line, 70)
+        print('cutquery:', len(cutresult))
+        for icut, record in enumerate(cutresult):
+            comment.append(record)
+
+    comment.append('END:SQLQUERY')
+    table.meta['COMMENT'] = comment
+
+    return table
+
+
 
 
 def get_storedquery(queryname=None):
@@ -186,11 +243,7 @@ def get_storedquery(queryname=None):
     """
 
     query0b = """
-    SELECT
-        COUNT(*)
-    FROM
-        allwise.main AS w
-    LIMIT 10;
+    SELECT COUNT(*) FROM allwise.main AS w LIMIT 10;
     """
 
     query0c = """
@@ -199,7 +252,7 @@ def get_storedquery(queryname=None):
     FROM
         allwise.main AS w
     WHERE
-        (w.ra > 1.0 AND w.ra < 2.0) AND
+        (w.ra > 1.0 OR w.ra < 2.0) AND
         (w.dec > -1.0 AND w.dec < 1.0)
     LIMIT 10;
     """
@@ -524,55 +577,11 @@ def get_storedquery(queryname=None):
     return query
 
 
-def cutquery(query, length):
-    """
-    Return query in a list of fixed sized character strings
-
-    suggest length = 70 so can be stored in FITS format COMMENT
-    from easyaccess codebase
-    """
-    return [query[0 + i:length + i] for i in range(0, len(query), length)]
 
 
-def table_metadata_sql(table, sqlfile=None, sqlquery=None):
-    """
-    Add SQL query information into Astropy table metadata and
-    can be output in a FITS header
 
 
-    """
-    # various ways to store the SQL query information in the FITS header
-    if sqlfile is not None:
-        table.meta['SQLFILE'] = sqlfile
-
-    # table.meta['SQL'] = query
-    # create list to store sql query
-    table.meta['SQLSTART'] = 'START:SQLQUERY'
-    isql = -1
-    for iline, line in enumerate(query.splitlines()):
-        isql = isql + 1
-        print(iline, ':', line)
-        # cat and wrap long sql lines at 67 chars since
-        # FITS has a max total record length of 80 and 13 characters
-        # are needed for the required information
-        # needs 12345678 = ''
-        #       1234567890123
-        cutresult = cutquery(line, 67)
-        print('cutquery:', len(cutresult))
-        for icut, record in enumerate(cutresult):
-            isql = isql + icut
-            if isql <= 99998:
-                table.meta['SQL' + '{:d}'.format(isql+1)] = record
-            if isql > 99998:
-                table.meta['SQL' + '{:d}'.format(99999)] = record
-
-    table.meta['SQLEND'] = 'END:SQLQUERY'
-    table.meta['QTIME'] = querytime
-
-    return table
-
-
-def get_args():
+def getargs():
     """
 
     """
@@ -592,7 +601,6 @@ def get_args():
     parser.add_argument("--outfile",
                   default=default_outfile,
                   help="Output filename")
-
 
     parser.add_argument("--sqlfile",
                   default=default_sqlfile,
@@ -619,28 +627,53 @@ def get_args():
     return args
 
 
+def clean_comments():
+    """
+    see https://github.com/andialbrecht/sqlparse/issues/410
+
+    """
+    import sqlparse
+    # help(sqlparse.sql)
+    new_query = []
+
+    print(len(query))
+    print(sqlparse.format(query, strip_comments=True))
+    query = sqlparse.format(query, strip_comments=True)
+    print(len(query))
+    print(sqlparse.format(query, strip_comments=True))
+    print(len(query))
+    query = sqlparse.format(query, strip_comments=True)
+    print(sqlparse.format(query, strip_comments=True))
+    print(len(query))
+    sys.exit()
+
+    for statement in sqlparse.parse(query):
+        # print(statement)
+        new_tokens = [stm for stm in statement.tokens
+                      if not isinstance(stm, sqlparse.sql.Comment)]
+
+        print(len(new_tokens))
+        for token in new_tokens:
+            print(token)
+
+        new_statement = sqlparse.sql.TokenList(new_tokens)
+        new_query.append(new_statement)
+
+    print(new_query)
+    print(len(new_query))
+    print(sqlparse.format("\n".join(new_query), strip_comments=True))
+    sys.exit()
+
+
 if __name__ == "__main__":
-
-    outpath = '/data/desardata4/PS/'
-    outpath = '/data/4most/IWG1/'
-
-    #outfilename = 'WISE_xmatch_PS1DR1_HZQv1.fits'
-    #outfilename = 'WISE_xmatch_PS1DR1_AGN.fits'
-
-    outfilename = 'WISE_xmatch_PS1DR1.fits'
-
-    #outfilename = 'PS1DR1.fits'
-    #outfile = outpath + outfilename
-
-
 
     print('Elapsed time(secs): ', time.time() - t0)
 
     # atpy support posgresl table io: http://atpy.readthedocs.io/en/stable/
     USING_atpy = False
-    debug = False
 
-    args = get_args()
+
+    args = getargs()
     # help(args)
     # print('args:', args)
     print('list the args and values')
@@ -649,29 +682,21 @@ if __name__ == "__main__":
     print()
 
     # Open and read the file as a single buffer
+    debug = args.debug
     sqlquery = args.sqlquery
     sqlfile = args.sqlfile
     storedquery = args.storedquery
     outfile = args.outfile
 
-    print('Output file:', outfile)
-
-    outpath = os.path.dirname(outfile)
-    print('Output file path:', outpath)
-
-    if not os.path.exists(outpath):
-       print('Output directory does not exist:', outpath)
-       print('Creating:', outpath)
-       os.makedirs(outpath)
-
-    # key=raw_input("Enter any key to continue: ")
+    print_table = False
 
     if sqlfile is not None:
         print('Reading: ',sqlfile)
         fh = open(sqlfile, 'r')
         sqlquery = fh.read()
         fh.close()
-        print('sqlquery:', sqlquery)
+        if debug:
+            print('sqlquery:', sqlquery)
 
     # sys.exit()
 
@@ -688,20 +713,22 @@ if __name__ == "__main__":
     if sqlquery is not None:
         query = sqlquery
 
-    print(query)
+    if debug:
+        print(query)
+        for iline, line in enumerate(query.splitlines()):
+            print(iline, ':', line)
 
-    for iline, line in enumerate(query.splitlines()):
-        print(iline, ':', line)
+
 
     print('host:', host)
-    print('query set')
+    print('query', len(query))
+    print(sqlparse.format(query, strip_comments=True))
     print('getting data...')
 
-    t1 = time.time()
-    result = sqlutil.get(query, db=db, host=host, strLength=64,
+    t1=time.time()
+    result = sqlutil.get(query, db=db, host=host,
                          user=user, password=password, asDict=True)
     querytime = time.time() - t1
-    print('Elapsed time(secs): ', time.time() - t0)
 
     if USING_atpy:
         result = atpy.Table('postgres', query=query, user=user, database='wsdb',
@@ -716,10 +743,8 @@ if __name__ == "__main__":
     print('len(table.colnames):', len(table.colnames))
     print('row[0]', table[0])
 
-    print_table = False
     if print_table:
         table.more()
-
 
     if debug:
         help(result)
@@ -732,10 +757,24 @@ if __name__ == "__main__":
     print('Elapsed time(secs): ', time.time() - t0)
     print('Data downloaded:', len(table), 'rows')
 
+    outpath = '/data/desardata4/PS/'
+    outpath = '/data/4most/IWG1/'
 
-    table_metadata_sql(table, sqlfile=sqlfile, sqlquery=sqlquery)
+    #outfilename = 'WISE_xmatch_PS1DR1_HZQv1.fits'
+    #outfilename = 'WISE_xmatch_PS1DR1_AGN.fits'
 
-    print('Table metadata')
+    outfilename = 'WISE_xmatch_PS1DR1.fits'
+
+    #outfilename = 'PS1DR1.fits'
+    #outfile = outpath + outfilename
+
+
+    table = table_add_sqlquery(table=table, query=query)
+
+    table.meta['QTIME'] = querytime
+
+    # print(table.meta)
+    print('Metadata')
     for key, value in table.meta.items():
         print('{0} = {1}'.format(key, value))
 
@@ -743,7 +782,10 @@ if __name__ == "__main__":
     print('Number of row:', len(table))
     print('Number of columns:', len(table.colnames))
 
-    table_metadata(table=table, verbose=True)
+    table.info()
+    table.info('stats')
+    table_metadata(table=table, verbose=verbose)
 
+    print('Saving:', outfile)
     table.write(outfile, overwrite=True)
     print('Elapsed time(secs): ', time.time() - t0)
